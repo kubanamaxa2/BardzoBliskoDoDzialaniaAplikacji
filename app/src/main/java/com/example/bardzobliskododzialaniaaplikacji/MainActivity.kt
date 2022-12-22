e40cd8b4ace5bd0114f66467f495f09c0d165d85
kubanamaxa2 <jakubniemczyk09@gmail.com>
2022-12-22 22:49:21 +0100

    Mam sortowania, teraz czas

--- app/src/main/java/com/example/bardzobliskododzialaniaaplikacji/MainActivity.kt
+++ app/src/main/java/com/example/bardzobliskododzialaniaaplikacji/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import java.util.Collections.swap
 import kotlin.random.Random
 
@@ -14,18 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var lostext = findViewById<TextView>(R.id.lostext)
-        var liczbalosowan = 0
-        val liczbalosedit = findViewById<EditText>(R.id.editTextNumberDecimal).text
+        val liczbaeledit = findViewById<EditText>(R.id.editTextNumberDecimal).text
+        val ilerazylos = findViewById<EditText>(R.id.editTextNumberDecimal2).text
         val losuj = findViewById<Button>(R.id.button)
-        val popv = findViewById<TextView>(R.id.popv)
         val popms = findViewById<TextView>(R.id.popms)
-        val speedv = findViewById<TextView>(R.id.speedv)
         val speedms = findViewById<TextView>(R.id.speedms)
-        val heapv = findViewById<TextView>(R.id.heapv)
         val heapms = findViewById<TextView>(R.id.heapms)
-        val scalv = findViewById<TextView>(R.id.scalv)
         val scalms = findViewById<TextView>(R.id.scalms)
-        fun sortbuble(arr: MutableList<Int>): MutableList<Int>{ //sortowanie bąbelkowe
+        val wstawms = findViewById<TextView>(R.id.wstawms)
+        var czas1: Long; var czas2: Long
+        //---------------------- sortowanie bąbelkowe ------------------------
+        fun sortbuble(arr: MutableList<Int>): MutableList<Int>{
             var swappedElements : Boolean
             var level = arr.size - 1
             do {
@@ -40,7 +40,9 @@ class MainActivity : AppCompatActivity() {
             } while (swappedElements)
             return arr
         }
-        fun quicksort(items:List<Int>):List<Int>{ //sortowanie szybkie
+
+        // ----------------------- sortowanie szybkie -----------------------
+        fun quicksort(items:List<Int>):List<Int>{
             if (items.count() < 2){
                 return items
             }
@@ -50,20 +52,140 @@ class MainActivity : AppCompatActivity() {
             val greater = items.filter { it > pivot }
             return quicksort(less) + equal + quicksort(greater)
         }
-        losuj.setOnClickListener {
-            var lista = arrayOf<Int>()
-            liczbalosowan = liczbalosedit.toString().toInt() -1
-            lostext.text = "| "
-            for(i in 0..liczbalosowan){
-                lista = lista.plus(0)
 
+        //----------------------sortowanie przez wstawianie-------------------
+        fun wstawsort(arr : MutableList<Int>)
+        {
+            for (i in 1 until arr.size) {
+                val current = arr[i]
+                var j = i - 1
+
+                while (j >= 0 && arr[j] > current) {
+                    arr[j + 1] = arr[j]
+                    j--
+                }
+
+                arr[j + 1] = current
             }
-            for(i in 0..liczbalosowan){
-                lista[i] = Random.nextInt(0,9)
-                lostext.text =lostext.text.toString() + lista[i].toString() + " | "
+        }
+
+        //-------------------Funkcja sortowania przez scalanie--------------------
+        fun merge(left: IntArray, right: IntArray): IntArray {
+            var leftIndex = 0
+            var rightIndex = 0
+            val result = IntArray(left.size + right.size)
+
+            for (i in result.indices) {
+                if (leftIndex >= left.size) {
+                    result[i] = right[rightIndex++]
+                } else if (rightIndex >= right.size) {
+                    result[i] = left[leftIndex++]
+                } else if (left[leftIndex] < right[rightIndex]) {
+                    result[i] = left[leftIndex++]
+                } else {
+                    result[i] = right[rightIndex++]
+                }
             }
-            popv.text = sortbuble(lista.toMutableList()).toString() //sortowanie bąbelkowe
-            speedv.text = quicksort(lista.toList()).toString()
+
+            return result
+        }
+        fun scalsort(array: IntArray): IntArray {
+            if (array.size <= 1) return array
+
+            val mid = array.size / 2
+            val left = scalsort(array.copyOfRange(0, mid))
+            val right = scalsort(array.copyOfRange(mid, array.size))
+
+            return merge(left, right)
+        }
+
+        //----------------Sortowanie przez kopcowanie (heapsort)-------------
+        fun partition(array: IntArray, low: Int, high: Int): Int {
+            val pivot = array[high]
+            var i = low - 1
+            for (j in low until high) {
+                if (array[j] <= pivot) {
+                    i++
+                    val temp = array[i]
+                    array[i] = array[j]
+                    array[j] = temp
+                }
+            }
+            val temp = array[i + 1]
+            array[i + 1] = array[high]
+            array[high] = temp
+            return i + 1
+        }
+
+        fun heapify(array: MutableList<Int>, n: Int, i: Int) {
+            var largest = i
+            val l = 2 * i + 1
+            val r = 2 * i + 2
+
+            if (l < n && array[l] > array[largest]) {
+                largest = l
+            }
+
+            if (r < n && array[r] > array[largest]) {
+                largest = r
+            }
+
+            if (largest != i) {
+                val temp = array[i]
+                array[i] = array[largest]
+                array[largest] = temp
+
+                heapify(array, n, largest)
+            }
+        }
+        fun heapsort(array: MutableList<Int>) {
+            // Budowanie kopca
+            for (i in array.size / 2 - 1 downTo 0) {
+                heapify(array, array.size, i)
+            }
+
+            for (i in array.size - 1 downTo 0) {
+                val temp = array[0]
+                array[0] = array[i]
+                array[i] = temp
+
+                heapify(array, i, 0)
+            }
+        }
+
+        //Funkcja losująca losową liste
+        fun losowanie(size: Int): MutableList<Int> {
+            val random = Random()
+            return List(size) { random.nextInt(1000) }.toMutableList()
+        }
+
+        //Funkcja licząca roznice miedzy pomiarami
+        fun calcTime(t1 : Long, t2 : Long) : Long
+        {
+            return t2 - t1
+        }
+
+
+        // -------------------- Działanie -----------------------------
+        losuj.setOnClickListener {
+            if(liczbaeledit.isEmpty() && ilerazylos.isEmpty()){
+                var lista = losowanie(liczbaeledit.toString().toInt() )
+
+
+
+
+
+                czas1 = System.currentTimeMillis()
+                    for(i in 0..ilerazylos.toString().toInt())
+                        sortbuble(lista) //sortowanie bąbelkowe
+
+
+                quicksort(lista.toList()) // sortowanie szybkie
+            }
+            else
+            {
+                Toast.makeText(this, "Wypełnij każde pole", Toast.LENGTH_SHORT).show()
             }
         }
     }
+}
